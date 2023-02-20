e2eb070e2265138fa16b7c0baf7a2feb4e021149
Unicode <73183149+whoisnicolasadm@users.noreply.github.com>
2023-02-20 18:59:58 -0300

    Update App.ts

--- App.ts
+++ App.ts
@@ -3,13 +3,9 @@ import { CompanyAccount } from "./Classes/CompanyAccount";
 import { NewAccountType } from "./Classes/NewAccountType";
 
 const usuario1: PersonalAccount = new PersonalAccount(1, "Nicolas", 123);
-//console.log(usuario1);
-// console.log(usuario1.getName())
-// usuario1.setName("Nicolas Dias")
-// console.log(usuario1.getName())
-  usuario1.withdraw(35)
-  usuario1.deposit(3)
-  //console.log(usuario1)
+usuario1.withdraw(35)
+usuario1.deposit(3)
+
 
 const newAccountUser: NewAccountType = new NewAccountType("Nicolas", 1232)
 newAccountUser.withdraw(40)
@@ -17,5 +13,5 @@ newAccountUser.deposit(40)
 console.log(newAccountUser)
 
 const company1: CompanyAccount = new CompanyAccount("Empresa1", 123);
-//console.log(company1)
-//company1.deposit()
+company1.getLoan(100)
+
